dd75424323a3915f3aebbf33e65e5fbf678fa045
shinseungyun <sstmddbs@naver.com>
2025-10-07 15:29:14 +0900

    수정

---
+++ 새 폴더/js/script.js
@@ -0,0 +1,79 @@
+document.addEventListener(`DOMContentLoaded`, function () { 
+
+    // 반응형 사이즈에 따라 스와이퍼 초기화 또는 제거
+    // 스와이퍼를 제거할때 그냥 반응형 사이즈만 설정하고 제거할 경우 스와이퍼 변수는 여전히 객체라는 존재로 남게된다
+
+    // 스와이퍼 변수 선언
+    let swiper = undefined;
+
+    function initSwiper() {
+        //윈도우 너비값을 변수에 저장
+        const windowWidth = window.innerWidth;
+
+        if (windowWidth >= 960 && swiper == undefined) {
+            // swiper
+            swiper = new Swiper(".mySwiper", {
+                loop: true,
+                effect: "fade",
+                speed: 1500,
+                autoplay: {
+                delay: 1500,  
+                },
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+            });
+        } else if(windowWidth < 960 && swiper != undefined) {
+            swiper.destroy();
+            swiper = undefined;
+        }
+    }
+
+    // 함수 전역 호출
+    initSwiper();
+
+    // 윈도우가 리사이즈 될때 스와이퍼 자동 반응 설정
+    window.addEventListener(`resize`, () => { 
+        initSwiper();
+    });
+
+    // submenu
+    const menuBtn = document.querySelector(`.menu_btn`);
+    const menuBarBox = document.querySelector(`.menu_bar_box`);
+
+    menuBtn.addEventListener(`click`, function () { 
+        this.classList.toggle(`active`);
+        // menuBarBox.classList.toggle(`active`);
+        // 토글 기능 두가지를 진행할 경우 브라우저 오류 발생할 수 있음 contains() 메서드를 사용하면 좋음
+        const menuHas = menuBtn.classList.contains(`active`);
+        // menuBtn에 active 클래스명이 있다
+        //contains() 메서드는 특정 문자열이 포함되어있는지 여부를 확인하고 포함되어있으면 true를 반환, 그렇지 않으면 false 를 반환
+
+        if (menuHas === true) {
+            menuBarBox.classList.add(`active`);  
+        } else {
+            menuBarBox.classList.remove(`active`);  
+        }
+
+    });
+
+    // scroll
+    // 200px 이상에서 .header_area,.header_logo,.menu_btn on 추가 / 그 밖에 경우  .header_area,.header_logo,.menu_btn on제거 (.header-area,.header-logo,.menu-btn 한번에 선택해서 진행해보기)
+
+    window.addEventListener(`scroll`, () => {
+        const scrollYoffset = window.scrollY;
+        console.log(scrollYoffset);
+
+        const headerSection = document.querySelectorAll(`.header_area, .header_logo, .menu_btn`);
+
+        for (const hs of headerSection){
+            if (scrollYoffset >= 200) {
+                hs.classList.add(`on`);
+            } else {
+                hs.classList.remove(`on`);
+            }
+        }
+    });
+
+});
